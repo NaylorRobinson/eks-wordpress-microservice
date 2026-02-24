ff0369fd279ddc098d2c31db50781c6b318b3fb7
NaylorRobinson <naylor.robinson@gmail.com>
2026-02-24 00:07:34 -0500

    Add Terraform configuration for AWS EKS module

---
+++ terrraform/main.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.0.0"
+
+  cluster_name    = var.cluster_name
+  cluster_version = "1.29"
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  cluster_endpoint_public_access = true
+
+  eks_managed_node_groups = {
+    standard-workers = {
+      instance_types = [var.node_instance_type]
+      min_size       = var.min_nodes
+      max_size       = var.max_nodes
+      desired_size   = var.desired_nodes
+    }
+  }
+}
